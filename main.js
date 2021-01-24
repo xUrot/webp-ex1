4382236f6c036b6f84c7a963027a9eeddde26f02
xUrot <72125414+xUrot@users.noreply.github.com>
2021-01-25 05:54:26 +0900

    webp-1

---
+++ main.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2-icon.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
